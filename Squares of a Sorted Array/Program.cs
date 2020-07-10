1cdf54ef2c93cff3127f9d66be569cf580c7e2d2
joydeeprony89 <joydeeprony89@gmail.com>
2020-07-10 10:22:15 +0200

    Add project files.

---
+++ Squares of a Sorted Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Squares_of_a_Sorted_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", SortedSquares(new int[] { -7, -3, 2, 3, 11 }));
+        }
+
+        public static int[] SortedSquares(int[] A)
+        {
+           for(int i = 0; i< A.Length; i++)
+            {
+                A[i] = A[i] * A[i];
+            }
+
+            Array.Sort(A);
+            return A;
+        }
+    }
+}
